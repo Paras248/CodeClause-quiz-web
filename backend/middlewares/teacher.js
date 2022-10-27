0697050c8a9af9ef78ebc9fd7e15824a0feec9cd
Paras248 <paraspatil248@gmail.com>
2022-10-28 01:27:07 +0530

    added isLoggedIn middleware

---
+++ backend/middlewares/teacher.js
@@ -0,0 +1,18 @@
+const BigPromise = require("./BigPromise");
+const CustomError = require("../utils/CustomError");
+const jwt = require("jsonwebtoken");
+const Teacher = require("../models/teacher");
+
+exports.teacherIsLoggedIn = BigPromise(async (req, res, next) => {
+    const token = req.cookies.token || req.header("Authorization").replace("Bearer ", "");
+
+    if (!token) {
+        return next(new CustomError("Please login first to access this page", 400));
+    }
+
+    const decoded = await jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = await Teacher.findById(decoded.id);
+
+    next();
+});
