c17e4b7216acdf05e3d4860bdf367dbf91d22200
Paras248 <paraspatil248@gmail.com>
2022-10-29 03:10:05 +0530

    made ui changes

--- frontend/src/components/UI/AuthHeader.js
+++ frontend/src/components/UI/AuthHeader.js
@@ -6,7 +6,7 @@ const AuthHeader = (props) => {
     return (
         <header className={styles.header}>
             <Link to="/" className={styles["title-link"]}>
-                <p>Quizie</p>
+                <p>Quizie.</p>
             </Link>
             <Link to={props.buttonRedirectTo}>
                 <button className={styles.button}>{props.buttonText}</button>
